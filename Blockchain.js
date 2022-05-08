a16b3d6963a7a9c1f0bce3108101f00e3bd72fdd
VK <vitaliy.krynytskyy@gmail.com>
2022-05-08 18:59:13 +0100

    Added chain validation

--- Blockchain.js
+++ Blockchain.js
@@ -19,6 +19,36 @@ class Blockchain {
     getLastBlock() {
         return this.chain[this.chain.length - 1];
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.getHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    isValid(blockchain = this) {
+        for (let i = 1; i < blockchain.chain.length; i++) {
+            const currentBlock = blockchain.chain[i];
+            const previousBlock = blockchain.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.getHash() || currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
 
 module.exports = Blockchain;
--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const Block = require('./Block');
 
 const myChain = new Blockchain();
 myChain.addBlock(new Block(Date.now().toString(), ['I am block 1']));
+
 console.log(myChain.chain);
+console.log(myChain.isValid());
