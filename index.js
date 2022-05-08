18edcec51052b97820435c5fcd020ffe92d646f7
VK <vitaliy.krynytskyy@gmail.com>
2022-05-08 16:07:13 +0100

    Created the first working iteration of the blockchain

---
+++ index.js
@@ -0,0 +1,6 @@
+const Blockchain = require('./Blockchain');
+const Block = require('./Block');
+
+const myChain = new Blockchain();
+myChain.addBlock(new Block(Date.now().toString(), ['I am block 1']));
+console.log(myChain.chain);
