9684d30e391f4a18afc2fc09a2882cfdeefe4852
VK <vitaliy.krynytskyy@gmail.com>
2022-05-09 16:23:59 +0100

    Updated helper usage

--- helpers.js
+++ helpers.js
@@ -1,7 +1,16 @@
 const crypto = require('crypto');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const MINT_KEY_PAIR = ec.genKeyPair();
+const MINT_PUBLIC_ADDRESS = MINT_KEY_PAIR.getPublic('hex');
+const holderKeyPair = ec.genKeyPair();
 
 const createSHA256Hash = message => crypto.createHash('sha256').update(message).digest('hex');
 
 module.exports = {
-    createSHA256Hash
+    createSHA256Hash,
+    MINT_KEY_PAIR,
+    MINT_PUBLIC_ADDRESS,
+    holderKeyPair
 }
